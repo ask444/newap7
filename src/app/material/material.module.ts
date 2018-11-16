a0ddaa66db056dc8d56938f36c187666483de111
ashok <ashok.ng786@gmail.com>
2018-11-16 08:13:38 +0530

    Initial commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatToolbarModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [MatButtonModule, MatToolbarModule],
+  exports: [MatButtonModule, MatToolbarModule],
+})
+export class MaterialModule { }
